1d056a7075db460ae081b9e14baeeee9150a473e
Serjino <58355478+Serjino@users.noreply.github.com>
2022-02-27 15:08:19 +0300

    calculate is ready

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import ReactPlayer from 'react-player/youtube'
 import { Button, Icon } from "@mui/material";
 import { BrowserRouter, Switch, Redirect, useLocation, useHistory, Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from "swiper/react";
-import {Pagination} from 'swiper';
+import { Pagination } from 'swiper';
+import NumberFormat from "react-number-format";
 import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -21,7 +22,7 @@ import slide9 from './misc/slider/slide9.jpg'
 import slide10 from './misc/slider/slide10.jpg'
 import slide11 from './misc/slider/slide11.jpg'
 
- const slides = [
+const slides = [
     {
         title: "",
         url: slide1
@@ -70,6 +71,25 @@ import slide11 from './misc/slider/slide11.jpg'
 
 
 function App() {
+    const [dimensions, setDimensions] = React.useState({
+        length: 0,
+        width: 0
+    })
+    const [square, setSquare] = React.useState(0)
+    const [price, setPrice] = React.useState(0)
+
+    const minPrice = 5000
+    const pricePerMetr = 1000
+    const minSquare = minPrice / pricePerMetr
+
+    React.useEffect(() => {
+        setSquare((dimensions.length * dimensions.width).toFixed(2))
+    }, [dimensions])
+
+    React.useEffect(() => {
+        minSquare < square ? setPrice((pricePerMetr * square).toFixed(2)) : setPrice(minPrice)
+    }, [square])
+
     return (
         <div className="main">
             <header className="header">
@@ -91,50 +111,112 @@ function App() {
                         repudiandae dignissimos neque quia magnam impedit distinctio sed ipsam asperiores quidem.
                         Repellendus quasi sunt aperiam ab numquam.
                     </div>
-                    <Button className="button" variant={"contained"}>Оставить заявку</Button>
+                    <div className="btn-wrapper">
+                        <Button className="button leave-order" variant={"contained"}>Оставить заявку</Button>
+                    </div>
                 </div>
             </div>
             <div className="section how-do-we-work">
                 <div className="section-content">
-                    <h1 className="section-heading">
+                    <h2 className="section-heading">
                         Как мы работаем
-                    </h1>
+                    </h2>
                     <div className="video-wrapper">
                         {/* <div className={'video'}> */}
-                        <ReactPlayer className="video" width={"100%"}
+                        <ReactPlayer className="video" width={"100%"} height={"450px"} controls
                             url="https://www.youtube.com/watch?v=_nwEuDVi8fs&feature=emb_logo"
                         />
                         {/* </div> */}
                     </div>
+                </div>
+            </div>
+            {/* <div className="slider-wrapper"> */}
+            <div className="section how-do-we-work">
+                <div className="section-content">
+                    <h2 className="section-heading">
+                        Галерея
+                    </h2>
                     <Swiper
-                    modules={[Pagination]}
-                    pagination
-                    className="mySwiper">
+                        modules={[Pagination]}
+                        pagination
+                        className="mySwiper">
+
                         {
-                            slides.map((slide : any) => 
+                            slides.map((slide: any) =>
                                 <SwiperSlide>
                                     <img src={slide.url}>
-                                        
+
                                     </img>
-                                    <img src={slide.title}/>
-                                    </SwiperSlide>
-                                )
+                                    <img src={slide.title} />
+                                </SwiperSlide>
+                            )
                         }
                     </Swiper>
-                    {/* <div className="text">
+                </div>
+            </div>
+            <div className="section calculate">
+                <div className="section-content">
+                    <h2 className="section-heading">
+                        Калькулятор стоимости
+                    </h2>
+                    <div className="calculator-wrapper">
+                        <div className="dimensions">
+                            <div className="dimension length">
+                                <div className="title">
+                                    Длина
+                                </div>
+                                <NumberFormat
+                                    className="input"
+                                    value={dimensions.length}
+                                    onValueChange={(v) => v.value.length > 0 && v.value != "." && setDimensions({
+                                        ...dimensions,
+                                        length: v.floatValue
+                                    })}
+                                />
+                            </div>
+                            <div className="multiply_sign">
+                                X
+                            </div>
+                            <div className="dimension width">
+                                <div className="title">
+                                    Ширина
+                                </div>
+                                <NumberFormat
+                                    className="input"
+                                    value={dimensions.width}
+                                    onValueChange={(v) => v.value.length > 0 && v.value != "." && setDimensions({
+                                        ...dimensions,
+                                        width: v.floatValue
+                                    })}
+                                />
+                            </div>
+                        </div>
+                        <div className="square">
+                            Площадь: <span className="result">{square} кв.м.</span>
+                        </div>
+                        <div className="price">
+                        {price > minPrice ? "Стоимость: " : "Мин. стоимость: "}  <span className="result">{price}р.</span>
+                        </div>
+                        <div className="btn-wrapper">
+                            <Button className="button leave-order" variant={"contained"}>Оставить заявку</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* </div> */}
+            {/* <div className="text">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et corrupti saepe fugit aliquid,
                         repudiandae dignissimos neque quia magnam impedit distinctio sed ipsam asperiores quidem.
                         Repellendus quasi sunt aperiam ab numquam.
                     </div> */}
-                </div>
-            </div>
+
         </div>
     )
 }
 
 ReactDOM.render(
     <BrowserRouter>
-        <App /> 
+        <App />
     </BrowserRouter>,
     document.getElementById('root')
 )
